bdf91db68f330713ef50e9f778cdf7807ce90f87
Vijay Kamble <92355658+vijay1155@users.noreply.github.com>
2023-03-05 22:03:14 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+
+    production: false
+    ,
+
+    cognito:{
+        userPoolId : 'ap-south-1_d4lXYZvMT',
+        userPoolWebClientId: '2r05rbd0s5hn53h5k571861eeu'
+    }
+};
